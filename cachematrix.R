2764847a322eb2fd491b9a42c42d901853a307b0
Pavlo Myronov <paul.myronov@gmail.com>
2014-07-26 21:46:24 +0300

    Some comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
 ## Following functions are designed to calculate inverted matrices
-## and avoid recalculation by getting caching results
+## and avoid unnecessary recalculation by getting caching results
 
 ## The first function creates a list of a functions to get and to change 
 ## values of original and inverted matrices
@@ -8,17 +8,17 @@ makeCacheMatrix <- function() {
 	minv <<- NULL
 	## The function "set" changes the value of original matrix. 
 	## It also sets the value of inverted matrix to zero because its value 
-	## needs recalculation
+	## needs to be recalculated
 	set <- function(y) {
 		morig <<- y
 		minv <<- NULL
 	}
-	## The function "get" gives a value of original matrix
+	## The function "get" returns a value of original matrix
 	get <- function() morig
 	## The function "setinv" records given value of inverted matrix into "minv" 
 	## variable
 	setinv <- function(matrixInversed) minv <<- matrixInversed
-	##function "getinv" gives the value of inverted matrix
+	##function "getinv" returns the value of inverted matrix
 	getinv <- function() minv
 	list(set = set, get = get,
     setinv = setinv,
@@ -30,13 +30,20 @@ makeCacheMatrix <- function() {
 ## If the value of inverted matrix was already calculated, the function returns cached value.
 
 cacheSolve <- function(x) {
-        m <- x$getinv()
+        ## Getting the value of inverted matrix and checking if it is null
+		m <- x$getinv()
 		if(!is.null(m)) {
+			## If the inverted matrix already was calculated the function
+			## returns the value from cash and stops working
 			message("No need to recalculate. Getting cached data")
 			return(m)
 		}
+		## If the inverted matrix wasn't calculated before, the function gets
+		## the value of original matrix, calculates the inverted matrix and 
+		## saves its value by using "setinv" function.
 		data <- x$get()
 		m <- solve(data)
 		x$setinv(m)
+		## The function returns calculated value of inverted matrix
 		m
 }
